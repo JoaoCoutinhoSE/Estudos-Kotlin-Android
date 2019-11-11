762e3e3e0779c8f126042067e47bc07c1e50201a
pandazim <jaotoledo74@gmail.com>
2019-11-11 15:17:11 -0400

    Botão com CardView feito, Agora falta a interação CodeBehind

--- app/src/main/java/com/ppd/pdzim/primeiropassos/MainActivity.kt
+++ app/src/main/java/com/ppd/pdzim/primeiropassos/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar().hide()
         setContentView(R.layout.activity_main)
 
 
