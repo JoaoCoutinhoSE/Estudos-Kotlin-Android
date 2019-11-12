572215fcca68569010e8669636ee71109037f469
pandazim <jaotoledo74@gmail.com>
2019-11-12 18:21:00 -0400

    du ve ae

--- app/src/main/java/com/ppd/pdzim/primeiropassos/MainActivity.kt
+++ app/src/main/java/com/ppd/pdzim/primeiropassos/MainActivity.kt
@@ -6,11 +6,25 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val username = "pandazim"
+    val password = "12345"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar().hide()
         setContentView(R.layout.activity_main)
+        button_login.setOnClickListener{
+            if (editText_username.text.toString() == username){
+                //usuario encontrado
+                if (editText_password.text.toString() == password){
+                    //Senha correta
+                }else {
+                    //Senha incorreta
+                }
+            }else {
+                //usuario não encontrado
 
+            }
+        }
 
     }
 }
