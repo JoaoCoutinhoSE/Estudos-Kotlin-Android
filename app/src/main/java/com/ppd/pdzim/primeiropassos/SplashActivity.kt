b259c74d3f98584039ec9ce559859589c0038cb6
pandazim <jaotoledo74@gmail.com>
2019-11-15 14:13:53 -0400

    Feito a SplashActivity que realiza a Intent no LoginaActivity

--- app/src/main/java/com/ppd/pdzim/primeiropassos/SplashActivity.kt
+++ app/src/main/java/com/ppd/pdzim/primeiropassos/SplashActivity.kt
@@ -1,14 +1,24 @@
 package com.ppd.pdzim.primeiropassos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val SPLASH_TIME_OUT:Long=5000
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
-        
+
+        Handler().postDelayed({
+           //Metodo que inicializa a activity Depois do tempo marcado
+            startActivity(Intent(this, LoginActivity::class.java))
+            //Finaliza a Splash
+            finish()
+            //Quantos segundos deve conter
+        }, SPLASH_TIME_OUT)
     }
 }
