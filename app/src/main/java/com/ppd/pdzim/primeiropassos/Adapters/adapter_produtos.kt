ff3011f5f371ba24ccd470ac8c8ef6a56e494da6
pandazim <jaotoledo74@gmail.com>
2019-11-17 19:18:49 -0400

    RecyclerView e implementações

---
+++ app/src/main/java/com/ppd/pdzim/primeiropassos/Adapters/adapter_produtos.kt
@@ -0,0 +1,8 @@
+package com.ppd.pdzim.primeiropassos.Adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.ppd.pdzim.primeiropassos.Models.Produto
+
+class adapter_produtos(val listaProdutos: MutableList<Produto>) : RecyclerView {
+
+}
---
+++ app/src/main/java/com/ppd/pdzim/primeiropassos/Models/Produto.kt
@@ -0,0 +1,10 @@
+package com.ppd.pdzim.primeiropassos.Models
+
+import android.widget.ImageView
+import android.widget.TextView
+
+data class Produto (
+//    var Imagem : ImageView,
+    var Nome : TextView,
+    var Preco: TextView
+)
